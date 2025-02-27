d3e019edf283fe336f2ff946e918bfa35a6c789b
ankit-548 <raghuwanshi.ankit548@gmail.com>
2025-02-27 22:15:59 +0530

    todo model

--- src/models/todos/todo.model.js
+++ src/models/todos/todo.model.js
@@ -1,5 +1,23 @@
 import mongoose from 'mongoose';
 
-const todoSchema = mongoose.Schema({});
+const todoSchema = mongoose.Schema({
+    content: {
+        type: String,
+        required: true,
+    },
+    complete: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    subTodos: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'subTodos'
+    }
+}, { timestamps: true });
 
 export const Todo = mongoose.model('Todo', todoSchema);
--- src/models/todos/user.model.js
+++ src/models/todos/user.model.js
@@ -1,5 +1,21 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: [true, 'password is required']
+    }
+}, {timestamps: true});
 
 export const User = mongoose.model('User', userSchema);
